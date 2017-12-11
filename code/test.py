362d840e0f1ab02d7b48d81c8bac26f91e00ee5b
Vatsal09 <vpan17@gmail.com>
2017-12-11 17:06:09 -0500

    New code

--- code/test.py
+++ code/test.py
@@ -1,23 +1,27 @@
 from gpiozero import *
-from signal import pause
+from time import sleep
 
 globalCounter = 0
 
 def printCounter():
 	print(str(globalCounter))
 
-motor1 = PWMOutputDevice(17)
+motor1 = PWMOutputDevice(17, frequency = 1000)
 
-while True:
-	motor1.on()
-	sleep(1)
-	globalCounter = globalCounter +1
-	motor1.off()
-	sleep(1)
-	# Number of pulses to test
-	if(globalCounter == 50):
-		printCounter()
-		break
+try:
+	while True:
+		motor1.off()
+		sleep(3)
+		motor1.on()
+		sleep(3)
+		globalCounter = globalCounter +1
+		# Number of pulses to test
+		if(globalCounter == 10):
+			printCounter()
+			break
+except KeyboardInterrupt:
+	motor1.close()
+	print("Exited through keyboard interupt")
 
 motor1.close()
-pause()
+
